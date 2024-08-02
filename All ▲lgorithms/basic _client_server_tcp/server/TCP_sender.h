e4ca64b93a92246002a13c1f562bbe6e790ec7e2
Boldrin1 <srzip74@gmail.com>
2024-08-02 17:46:50 -0300

    02/08/2024

---
+++ All ▲lgorithms/basic _client_server_tcp/server/TCP_sender.h
@@ -0,0 +1,21 @@
+/*
+================================
+Name         : TCP_Sender.h
+Author       : GodisCOOL
+Description  : Simple TCP sender. Expected input {IP address, port, number of parts, file name}
+================================
+*/
+
+#ifndef C_1_TCP_SENDER_H
+#define C_1_TCP_SENDER_H
+
+#include <studio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#endif //C_1_TCP_SENDER_H
